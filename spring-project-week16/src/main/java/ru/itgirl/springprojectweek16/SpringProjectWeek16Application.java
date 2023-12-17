b6d52a4348931c1131f4f3508a590c227d87a366
Sokolova_ES <shestak.ekaterina@gmail.com>
2023-12-17 16:03:59 +0300

    adding all study projects in one repository

---
+++ spring-project-week16/src/main/java/ru/itgirl/springprojectweek16/SpringProjectWeek16Application.java
@@ -0,0 +1,13 @@
+package ru.itgirl.springprojectweek16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectWeek16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectWeek16Application.class, args);
+	}
+
+}
